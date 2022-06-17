4c0818e4ff7f44fe482466ae4cbcc0fed7a26573
Zachary Warnes <zacharywarnes@gmail.com>
2022-06-16 21:10:31 -0700

    after tutor session, was refreshed on how to write for loops that can generate random values. Added this to js scrips as well as cleaned up defined variables. Added js. script to ensure that once a character type is selected and entered into concat array, that character type is used and a randrom character type from that variable is applied to the password

--- script.js
+++ script.js
@@ -3,17 +3,15 @@
 //letters, numbers, and symbols or 'special characters'.
 
 
-var length = [""]
 var upperCaseAlpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 var lowerCaseAlpha = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
 var numbers = ["0","1","2","3","4","5","6","7","8","9"];
-var symbols = ["!","@","#","$","%","^","&","*","_","-","+","="];
+var symbols = ["!","@","#","$","%","^","&","*","-","+","="];
 
 function generatePassword() {
 
-  var lengthChoice = window.prompt("How many characters would you like your password to be. Chose a number between 8 and 128");
-  var passwordLength = [lengthChoice];
-
+  var passwordLength = window.prompt("How many characters would you like your password to be. Chose a number between 8 and 128");
+  
   if (passwordLength < 8) {
     window.alert ("Incorrect Choice! Please choose a number between 8 and 128");
     return;
@@ -25,20 +23,16 @@ function generatePassword() {
   }
   console.log(passwordLength);
   
-  var numbersChoice = window.confirm ("Click OK to use numbers in your password");
-  var numbersEl = [numbersChoice];
+  var numbersEl = window.confirm ("Click OK to use numbers in your password");
   console.log(numbersEl);
 
-  var upperCaseChoice = window.confirm ("Click OK to use Upper Case letters in your password");
-  var upperCaseEl = [upperCaseChoice];
+  var upperCaseEl = window.confirm ("Click OK to use Upper Case letters in your password");
   console.log(upperCaseEl);
 
-  var lowerCaseChoice = window.confirm ("Click OK to use Lower Case letters in your password ");
-  var lowerCaseEl = [lowerCaseChoice];
+  var lowerCaseEl = window.confirm ("Click OK to use Lower Case letters in your password ");
   console.log(lowerCaseEl);
 
-  var symbolsChoice = window.confirm ("Click OK to use symbols in your password");
-  var symbolsEl = [symbolsChoice];
+  var symbolsEl = window.confirm ("Click OK to use symbols in your password");
   console.log(symbolsEl);
 
 
@@ -48,40 +42,38 @@ function generatePassword() {
 
   var charactersCombined = []
 
-  if (passwordLength) {
-    charactersCombined =  charactersCombined.concat(length);
-  } 
+  var userPassword = ""
+
   if (numbersEl) {
     charactersCombined = charactersCombined.concat(numbers);
+    userPassword = userPassword + numbers[Math.floor(Math.random()*numbers.length)];
   }
   if (upperCaseEl) {
     charactersCombined = charactersCombined.concat(upperCaseAlpha);
+    userPassword = userPassword + upperCaseAlpha[Math.floor(Math.random()*upperCaseAlpha.length)];
   }
   if (lowerCaseEl) {
     charactersCombined = charactersCombined.concat(lowerCaseAlpha);
+    userPassword = userPassword + lowerCaseAlpha[Math.floor(Math.random()*lowerCaseAlpha.length)];
   }
   if (symbolsEl) {
     charactersCombined = charactersCombined.concat(symbols);
+    userPassword = userPassword + symbols[Math.floor(Math.random()*symbols.length)];
   }
-
-// Password Variable
-  // var userPassword = ""
-
-
-
-  // FOr Loop to Randomize FInal user choices
+console.log(userPassword);
   
-  // for (var i = 0; i < passwordLength; i++)
-  // userPassword + charactersCombined all that mathfloor stuff...
-
-
+for (var i = userPassword.length; i < passwordLength; i++){
+  userPassword = userPassword + charactersCombined[Math.floor(Math.random()*charactersCombined.length)];
   
+}
+  
+
 
   //Adding that character to a building password string;
 
   // Return the completed string;
 
-  // return userPassword;
+  return userPassword;
   
 
 }
@@ -100,4 +92,4 @@ function writePassword() {
 }
 
 // Add event listener to generate button
-// generateBtn.addEventListener("click", writePassword);
+generateBtn.addEventListener("click", writePassword);
